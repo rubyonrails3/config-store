a97b01b82ce575112420fd10b4b2a42f7dfa925a
Ismail Akram <rubyonrails3@gmail.com>
2015-02-22 16:15:55 +0500

    Fixed action wrong cache key update

--- app/actions/updates_active_config.rb
+++ app/actions/updates_active_config.rb
@@ -2,7 +2,7 @@ class UpdatesActiveConfig < Struct.new(:services)
 
   def update config:, params: {}
     result = config.update!(params)
-    Rails.cache.write(config.key, config.value) unless Rails.env.test?
+    Rails.cache.write("#{Rails.env}/#{config.key}", config.value) unless Rails.env.test?
     result
   end
 end
